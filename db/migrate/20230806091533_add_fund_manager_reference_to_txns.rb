c54e0471db8a4ba3972f21180fa12a31b0ab16d7
Avijit Kuila <avijitkuila5@gmail.com>
2023-08-09 01:07:48 +0530

    fix

--- db/migrate/20230806091533_add_fund_manager_reference_to_txns.rb
+++ db/migrate/20230806091533_add_fund_manager_reference_to_txns.rb
@@ -1,4 +1,4 @@
-class AddFundManagerReferenceToTransactios < ActiveRecord::Migration[7.0]
+class AddFundManagerReferenceToTxns < ActiveRecord::Migration[7.0]
   def change
     add_column :txns, :fund_manager_id, :string
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
